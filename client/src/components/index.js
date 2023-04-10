159f429bcaa505b987ff9cd270120a7b106968ca
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-04-10 19:26:43 +0200

    Termino pagina Customizer

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,13 @@
 import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+import ColorPicker from "./ColorPicker";
 
-export { CustomButton };
+export { 
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab,
+ };
