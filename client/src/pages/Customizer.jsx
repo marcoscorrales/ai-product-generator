376804bc5675c5276be0174cb0afe0a0c67c94d8
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-04-15 16:18:54 +0200

    Arreglo error al seleccionar tu propio logo

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -22,22 +22,26 @@ const Customizer = () => {
   const [activeFilterTab, setActiveFilterTab] = useState({
     logoShirt: true,
     stylishShirt: false,
-  });
+  })
 
-  // show tab content depending on the active tab
+  // show tab content depending on the activeTab
   const generateTabContent = () => {
     switch (activeEditorTab) {
       case "colorpicker":
-        return <ColorPicker/>
+        return <ColorPicker />
       case "filepicker":
         return <FilePicker
           file={file}
           setFile={setFile}
           readFile={readFile}
         />
-        case "aipicker":
-        return <AIPicker/>
-
+      case "aipicker":
+        return <AIPicker 
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />
       default:
         return null;
     }
@@ -48,22 +52,23 @@ const Customizer = () => {
 
     state[decalType.stateProperty] = result;
 
-    if(!activeEditorTab[decalType.isFilterTab]){
-      handleActiveFilterTab(decalType.filterTab);
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
     }
   }
 
   const handleActiveFilterTab = (tabName) => {
     switch (tabName) {
       case "logoShirt":
-        state.isLogoTexture = !activeFilterTab[tabName];
+          state.isLogoTexture = !activeFilterTab[tabName];
         break;
       case "stylishShirt":
-        state.isFullTexture = !activeFilterTab[tabName];
+          state.isFullTexture = !activeFilterTab[tabName];
         break;
       default:
         state.isLogoTexture = true;
         state.isFullTexture = false;
+        break;
     }
   }
 
@@ -80,18 +85,18 @@ const Customizer = () => {
       {!snap.intro && (
         <>
           <motion.div
-          key="custom"
-          className='absolute top-0 left-0 z-10'
-          {...slideAnimation('left')}
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
           >
-            <div className='flex items-center min-h-screen'>
+            <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                <Tab
-                  key={tab.name}
-                  tab={tab}
-                  handleClick={() => setActiveEditorTab(tab.name)}
-                />
+                  <Tab 
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
 
                 {generateTabContent()}
@@ -100,30 +105,30 @@ const Customizer = () => {
           </motion.div>
 
           <motion.div
-            className='absolute z-10 top-5 right-5'
+            className="absolute z-10 top-5 right-5"
             {...fadeAnimation}
           >
-            <CustomButton
-              type='filled'
-              title='Go Back'
+            <CustomButton 
+              type="filled"
+              title="Go Back"
               handleClick={() => state.intro = true}
-              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
             />
           </motion.div>
 
           <motion.div
             className='filtertabs-container'
-            {...slideAnimation('up')}
+            {...slideAnimation("up")}
           >
             {FilterTabs.map((tab) => (
               <Tab
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
               />
-              ))}
+            ))}
           </motion.div>
         </>
       )}
